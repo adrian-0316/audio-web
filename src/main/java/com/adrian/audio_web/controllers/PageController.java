1b5f1f3386a8f499cbefa5ffd1a8c54725b70281
Adrian <112898743+adrian-0316@users.noreply.github.com>
2026-02-26 22:23:52 +0300

    Controller added

---
+++ src/main/java/com/adrian/audio_web/controllers/PageController.java
@@ -0,0 +1,17 @@
+package com.adrian.audio_web.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/services")
+    public String services() {
+        return "services";
+    }
+}
